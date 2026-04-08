ad477617cfcedc1c9238a9a4763511f1c81c6fe4
Michel Detiilli <detiillimichel@gmail.com>
2026-04-08 17:04:53 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,21 +2,19 @@
 
 const CACHE_NAME = 'vibe-oio-v1';
 const urlsToCache = [
-  './',
-  './index.html',
-  './css/style.css',
-  './css/theme.css',
-  './js/script.js',
-  './js/nav.js'
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/css/theme.css',
+  '/js/script.js',
+  '/js/nav.js'
 ];
 
 // Instala o Service Worker e salva os arquivos principais no celular
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
@@ -24,9 +22,6 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        // Retorna o que está no cache ou faz a requisição na internet
-        return response || fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
